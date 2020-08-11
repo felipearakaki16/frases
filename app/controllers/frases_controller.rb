6be2319dbcbb7fc4d2b0891ef8d206ef113b8ff6
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-08-11 11:48:09 -0300

    Devise installed

--- app/controllers/frases_controller.rb
+++ app/controllers/frases_controller.rb
@@ -15,26 +15,34 @@ class FrasesController < ApplicationController
 
   def show
     # tem que ser recursivo!
-    if !Frase.friendly.find(params[:id])
+    if Frase.friendly.where('tag': params[:id]).present?
+      frase = Frase.friendly.find(params[:id])
+    else
       url = "http://quotes.toscrape.com/tag/#{params[:id]}/"
       
       html_file = open(url).read
       html_doc = Nokogiri::HTML(html_file)
       # frase = JSON.generate(html_doc)
       
-      render json: { 
-                      "quotes": [
-                        {
-                          "quote": html_doc.search('.quote .text').first.text,
-                          "author": html_doc.search('.quote .author').first.text,
-                          "author-about": html_doc.search('.quote span a').first,
-                          "tags": html_doc.search('.quote .tags a').text.split
-                        }
-                      ]
-                    }
-    else
-
+      frase = Frase.create(
+                    "tag": params[:id],
+                    "quote": html_doc.search('.quote .text').first.text,
+                    "author": html_doc.search('.quote .author').first.text,
+                    "about": html_doc.search('.quote span a').first,
+                    "tags": html_doc.search('.quote .tags a').text.split
+                  )
+      frase.save
     end
+        render json: { 
+                        "quotes": [
+                          {
+                            "quote": frase.quote,
+                            "author": frase.author,
+                            "author-about": frase.about,
+                            "tags": frase.tags
+                          }
+                        ]
+                      }
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :frases, only: [ :index, :show ]
 end
